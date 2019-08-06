bde2ff470a0e1224681b24730c6e00985c7c4412
Mario Mazzola <mmmilione@gmail.com>
2019-08-06 11:05:49 +0200

    base

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,12 +30,6 @@ let formContent =  "<div class='modal-content'><h4 class = 'blue-text'>Add New B
     formContent += "<div class='file-path-wrapper'><input class='file-path validate' type='text'></div></div>";
     formContent += "<div class='center-align'><a href='#' class='btn-large white blue-text' onclick = 'submit()'><i class='fas fa-plus'></i></a></div></form></div>";
 
-renderProgress=(e)=>{
-    	const percent = document.querySelector('#percentage');
-    	const progressDone = ((e.loaded / e.total) * 100);
-    	percent.innerHTML = progressDone.toFixed(0);
-}
-
 showForm = () => {
       document.querySelector('#addBookModal').innerHTML = formContent;
       const elems = document.querySelector('#addBookModal');
@@ -76,44 +70,6 @@ getBooks = () =>{
 }
 
 submit = () =>{
-
-      //create new Http Request and open it
-      const xhr = new XMLHttpRequest();
-      xhr.open('POST', '/upload.html', true);
-
-      //Create new FormData Object
-      let formdata = new FormData();
-
-      //Get inputs to be submitted and append the to formdata
-      const title = document.querySelector('#title').value;
-      formdata.append('title', title);
-      const author = document.querySelector('#author').value;
-      formdata.append('author', author);
-      var photo = document.getElementById("foto").files[0];
-      formdata.append('photo', photo);
-
-      //Listen for Upload progress and fires renderProgressFunction to show progress to user
-      xhr.upload.addEventListener('progress', renderProgress, false);
-
-      /*List for request state changes. When complete (status 200 and state 4)
-      diplay error message if something went wrong. Else diplay success message */
-      xhr.onreadystatechange = function(){
-        if(xhr.status == 500){
-          document.querySelector('#addBookModal').innerHTML = errorMessage;
-          return;
-        }
-        if(xhr.readyState == 4 && xhr.status == 200) {
-          if(xhr.responseText.includes('Error')){
-              console.log(xhr.responseText);
-              document.querySelector('#addBookModal').innerHTML = errorMessage;
-              return;
-          }
-          console.log(JSON.parse(xhr.responseText));
-          document.querySelector('#addBookModal').innerHTML = success;
-        }
-      }
-
-      //send http post request to submit FormData
-      xhr.send(formdata);
-      document.querySelector('#addBookModal').innerHTML = progress;
+      //do something
+      console.log('upload stuff');
     }
